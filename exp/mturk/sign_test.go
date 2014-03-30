62e8ffff4e687d4ea57ac7ebe0f1e587e1f6a4c9
Matt Heath <matt@mattheath.com>
2014-03-30 21:29:32 +0100

    Use gocheck.* syntax in all tests

--- exp/mturk/sign_test.go
+++ exp/mturk/sign_test.go
@@ -3,7 +3,7 @@ package mturk_test
 import (
 	"github.com/goamz/goamz/aws"
 	"github.com/goamz/goamz/exp/mturk"
-	. "github.com/motain/gocheck"
+	"github.com/motain/gocheck"
 )
 
 // Mechanical Turk REST authentication docs: http://goo.gl/wrzfn
@@ -11,9 +11,9 @@ import (
 var testAuth = aws.Auth{"user", "secret", ""}
 
 // == fIJy9wCApBNL2R4J2WjJGtIBFX4=
-func (s *S) TestBasicSignature(c *C) {
+func (s *S) TestBasicSignature(c *gocheck.C) {
 	params := map[string]string{}
 	mturk.Sign(testAuth, "AWSMechanicalTurkRequester", "CreateHIT", "2012-02-16T20:30:47Z", params)
 	expected := "b/TnvzrdeD/L/EyzdFrznPXhido="
-	c.Assert(params["Signature"], Equals, expected)
+	c.Assert(params["Signature"], gocheck.Equals, expected)
 }
