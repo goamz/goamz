4278afb1b8298511f0ffd4cd8d804e1da278606d
Boyan Dimitrov <boyan.dimitrov@hailocab.com>
2014-04-05 16:27:37 +0100

    Adding NewAuth method

--- aws/aws.go
+++ aws/aws.go
@@ -192,6 +192,16 @@ func (a *Auth) Expiration() time.Time {
 	return a.expiration
 }
 
+//To be used with other APIs that return auth credentials such as STS
+func NewAuth(accessKey, secretKey, token string, expiration time.Time) *Auth {
+	return &Auth{
+		AccessKey:  accessKey,
+		SecretKey:  secretKey,
+		token:      token,
+		expiration: expiration,
+	}
+}
+
 // ResponseMetadata
 type ResponseMetadata struct {
 	RequestId string // A unique ID for tracking the request
