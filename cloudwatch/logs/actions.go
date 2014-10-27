95a9a18c489595437c8804ef31ece53ae090c208
Benton Roberts <broberts@mdsol.com>
2014-10-27 12:12:40 -0700

    cloudwatch logs - refactor construction of queries with LogGroup name parameter

--- cloudwatch/logs/actions.go
+++ cloudwatch/logs/actions.go
@@ -6,28 +6,26 @@ import "encoding/json"
 
 // CreateLogGroup - see http://goo.gl/IXnbI6
 func (l *CloudWatchLogs) CreateLogGroup(name string) error {
-	query := NewEmptyQuery().AddLogGroupName(name)
-	_, err := l.doQuery(target("CreateLogGroup"), query)
+	_, err := l.doQuery(target("CreateLogGroup"), NewGroupQuery(name))
 	return err
 }
 
 // CreateLogStream - see http://goo.gl/1IEVYN
 func (l *CloudWatchLogs) CreateLogStream(groupName, streamName string) error {
-	query := NewEmptyQuery().AddLogGroupName(groupName).AddLogStreamName(streamName)
+	query := NewGroupQuery(groupName).AddLogStreamName(streamName)
 	_, err := l.doQuery(target("CreateLogStream"), query)
 	return err
 }
 
 // DeleteLogGroup - see http://goo.gl/jz4l6O
 func (l *CloudWatchLogs) DeleteLogGroup(name string) error {
-	query := NewEmptyQuery().AddLogGroupName(name)
-	_, err := l.doQuery(target("DeleteLogGroup"), query)
+	_, err := l.doQuery(target("DeleteLogGroup"), NewGroupQuery(name))
 	return err
 }
 
 // DeleteLogStream - see http://goo.gl/izxZRW
 func (l *CloudWatchLogs) DeleteLogStream(groupName, streamName string) error {
-	query := NewEmptyQuery().AddLogGroupName(groupName).AddLogStreamName(streamName)
+	query := NewGroupQuery(groupName).AddLogStreamName(streamName)
 	_, err := l.doQuery(target("DeleteLogStream"), query)
 	return err
 }
@@ -56,7 +54,7 @@ func (l *CloudWatchLogs) DescribeLogStreams(
 	*DescribeLogStreamsResult, error) {
 
 	// define query
-	query := NewEmptyQuery().AddLogGroupName(groupName)
+	query := NewGroupQuery(groupName)
 	query.AddLimit(limit).AddLogGroupNamePrefix(prefix).AddNextToken(token)
 	// perform query
 	body, err := l.doQuery(target("DescribeLogStreams"), query)
@@ -96,9 +94,9 @@ func (l *CloudWatchLogs) GetLogEvents(groupName, streamName string,
 	startTime, endTime int64, nextToken string, limit int, startFromHead bool) (
 	*GetLogEventsResult, error) {
 	// define query
-	query := NewEmptyQuery().AddStartFromHead(startFromHead)
-	query.AddLogGroupName(groupName).AddLogStreamName(streamName)
-	query.AddLimit(limit).AddStartTime(startTime).AddEndTime(endTime)
+	query := NewGroupQuery(groupName).AddLogStreamName(streamName)
+	query.AddStartFromHead(startFromHead).AddLimit(limit)
+	query.AddStartTime(startTime).AddEndTime(endTime)
 	// perform query
 	body, err := l.doQuery(target("GetLogEvents"), query)
 	if err != nil {
@@ -113,16 +111,14 @@ func (l *CloudWatchLogs) GetLogEvents(groupName, streamName string,
 // PutRetentionPolicy - see http://goo.gl/ktuPXT
 func (l *CloudWatchLogs) PutRetentionPolicy(
 	groupName string, retentionInDays int32) error {
-	query := NewEmptyQuery()
-	query.AddLogGroupName(groupName).AddRetentionInDays(retentionInDays)
+	query := NewGroupQuery(groupName).AddRetentionInDays(retentionInDays)
 	_, err := l.doQuery(target("PutRetentionPolicy"), query)
 	return err
 }
 
 // DeleteRetentionPolicy - see http://goo.gl/rmH58u
 func (l *CloudWatchLogs) DeleteRetentionPolicy(name string) error {
-	query := NewEmptyQuery().AddLogGroupName(name)
-	_, err := l.doQuery(target("DeleteRetentionPolicy"), query)
+	_, err := l.doQuery(target("DeleteRetentionPolicy"), NewGroupQuery(name))
 	return err
 }
 
@@ -130,8 +126,7 @@ func (l *CloudWatchLogs) DeleteRetentionPolicy(name string) error {
 func (l *CloudWatchLogs) PutMetricFilter(filterName, filterPattern,
 	groupName string, metrics []MetricTransformation) error {
 	// define query
-	query := NewEmptyQuery()
-	query.AddLogGroupName(groupName).AddMetricTransformations(metrics)
+	query := NewGroupQuery(groupName).AddMetricTransformations(metrics)
 	query.AddFilterName(filterName).AddFilterPattern(filterPattern)
 	// perform query
 	_, err := l.doQuery(target("PutMetricFilter"), query)
@@ -143,7 +138,7 @@ func (l *CloudWatchLogs) DescribeMetricFilters(
 	groupName, prefix string, limit int, token string) (
 	*DescribeMetricFiltersResult, error) {
 	// define query
-	query := NewEmptyQuery().AddLogGroupName(groupName)
+	query := NewGroupQuery(groupName)
 	query.AddLimit(limit).AddFilterNamePrefix(prefix).AddNextToken(token)
 	// perform query
 	body, err := l.doQuery(target("DescribeMetricFilters"), query)
@@ -176,7 +171,7 @@ func (l *CloudWatchLogs) TestMetricFilter(
 
 // DeleteMetricFilter - see http://goo.gl/STP84A
 func (l *CloudWatchLogs) DeleteMetricFilter(groupName, filterName string) error {
-	query := NewEmptyQuery().AddLogGroupName(groupName).AddFilterName(filterName)
+	query := NewGroupQuery(groupName).AddFilterName(filterName)
 	_, err := l.doQuery(target("DeleteMetricFilter"), query)
 	return err
 }
